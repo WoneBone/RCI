43110966a4833d36fc4c691d7a7c110f6de2baec
WoneBone <nuno.g.tribolet.de.abreu@tecnico.ulisboa.com>
2024-02-27 17:56:15 +0000

    Alterações ao join. Não sei se funcionam

--- com.c
+++ com.c
@@ -4,15 +4,30 @@ extern int errcode;
 
 int join(int ring, int id, struct addrinfo *res){
 	int fd = socket(AF_INET, SOCK_DGRAM, 0);
-	char s[500];
+	char s[1000], *t;
+	int rID;
+	char ip[50], port[50];
+	
 	if(fd == -1) exit(1); //erro
 						  //
 	sprintf(s,"NODES %d\n", ring);					  
 	errcode = sendto(fd, s, strlen(s),0, res->ai_addr, res->ai_addrlen);
 	if(errcode == -1) exit(1);//error
 							
-	errcode = recvfrom(fd, s, 500,0, NULL, NULL);
+	errcode = recvfrom(fd, s, 1000,0, NULL, NULL);
 	if(errcode == -1) exit(1); /*error*/
 
 	puts(s);
+	t = strtok(s, "\n");
+	if(t == NULL) return id;
+	while(sscanf(t, "%d %s %s", &rID, ip, port) == 3){
+		if(id == rID){
+			id = join(ring, (id + 1)%100, res);
+			break;
+		}
+		t = strtok(NULL, "\n");
+		if(t == NULL) break;
+	}
+
+	return id;
 }
