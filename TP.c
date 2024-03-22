61bfc0af6c919a7094866d423758fb3dc147d932
WoneBone <nuno.g.tribolet.de.abreu@tecnico.ulisboa.com>
2024-03-22 00:08:17 +0000

    Acho que o ficheiro auziliar está. Agora é só substituir a sua utilização everywhere

--- TP.c
+++ TP.c
@@ -1,7 +1,5 @@
-typedef struct PATH{
-	unsigned char size;
-	unsigned char route[16];
-}path;
+#include "header.h"
+#include "tp.h"
 
 path RTable[100][100];
 path SPTable[100];
@@ -46,20 +44,119 @@ unsigned char source(path p){
 	}
 	return -1;
 }
-
-void updateRT(path p){
-	
+/*Função de comparação de paths
+ * Retorna -1 se p1 < p2
+ * retorna 0 se p1 = p2
+ * retorna 1 se p1 > p2
+ * renorna 2 se p1 e p2 tiverem o mesmo tamanho mas forem caminhos diferentes*/
+int patheq(path p1, path p2){
+	if(p1.size > p2.size) return 1;
+	else if(p1.size < p2.size) return -1;
+	else if(p1.size == p2.size){
+		for(int i = 0; i < p1.size; i++)
+			if(p1.route[i] != p2.route[i]) return 2;
+	}
+	return 0;
 }
 
+void updateRT(path p){
+	unsigned char dst = dest(p);
+	unsigned char src = source(p);
 
+	if(p.size <= 0 || dst == (unsigned char) -1 || src == (unsigned char) -1){
+		updateRT_empty(p);
+		return;
+	}
 
+	RTable[dst][src] = p;
 
+	if(source(SPTable[dst]) == src || (SPTable[dst].size > 0 && p.size < SPTable[dst].size))
+		updateSP(dst);
 
+}
 
+void updateSP(unsigned char row){
+	path p = SPTable[row];
+	unsigned char src = source(p);
+	unsigned char f = 0;
 
+	for(int i = 0; i < 100; i++){
+		//Disparidade entre caminho na SP e na RT. Mesma Source, caminho diferente;
+		if(src == i && patheq(SPTable[row], RTable[row][i]) != 0){
+			SPTable[row] = RTable[row][i];
+			i = 0;
+			f = 1;
+		}
+		//Caminho Válido na RT e SP com caminho vazio
+		else if (RTable[row][i].size > 0 && SPTable[row].size <= 0) {
+			SPTable[row] = RTable[row][i];
+			src = i;
+			f = 1;
+		}
+		//Caminho válido na RT de tamanho inferior ao na RT
+		else if( RTable[row][i].size < SPTable[row].size && RTable[row][i].size > 0){
+			SPTable[row] = RTable[row][i];
+			src = i;
+			f = 1;
+		}
+	}
+	
+	if(f == 1)
+		adj_route(SPTable[row]);
+}
 
+void adj_route(path p){
+	if(succ.id >= 0){
+		sendRoute(p, succ.fd);
+	}
+	if(pred.id >= 0){
+		sendRoute(p, pred.fd);
+	}
+}
 
+void sendRoute(path p, int fd){
+	char buffer[200], element[4];
+	unsigned char src = source(p);
+	unsigned char dst = dest(p);
+	int n = 0;
+	if(p.size <= 0){
+		sprintf(buffer, "ROUTE %d %d\n", mid, dst);
+		n =  write(fd, buffer, strlen(buffer));
+		if(n == -1)/*error*/ exit(1);
+		return;
+	}
 
+	sprintf(buffer, "ROUTE %d %d %d", mid, dst, mid);
+	for(int i = 0; i < p.size; i++){
+		sprintf(element, "-%02d", p.route[i]);
+		strcat(buffer, element);
+	}
+	strcat(buffer, "\n");
+	n = write(fd, buffer, strlen(buffer));
+	if(n == -1) /*exit*/ exit(1);
+}
 
+void updateRT_empty(path p){
+	
+	RTable[p.route[1]][p.route[0]] = p;
+	if(p.route[0] == source(SPTable[p.route[1]])){
+		SPTable[p.route[1]] = p;
+		updateSP(p.route[1]);
+		
+		if(SPTable[p.route[1]].size <= 0){
+			adj_route(SPTable[p.route[1]]);
+		}
+	}
+}
 
-
+void removeCol(unsigned char id){
+	path p;
+	for(int i = 0; i < 100; i++){
+		//Path vazio
+		p.size = -1;
+		p.route[0] = id;
+		p.route[1] = i;
+		//Põe na tabela
+		updateRT_empty(p);
+	}
+}
---
+++ tp.h
@@ -0,0 +1,57 @@
+#ifndef tp_header
+#define tp_header
+
+typedef struct PATH{
+	unsigned char size;
+	unsigned char route[16];
+}path;
+
+extern path RTable[100][100];
+extern path SPTable[100];
+extern unsigned char ETable[100];
+
+
+#endif
+#include "header.h"
+#include "tp.h"
+
+extern path RTable[100][100];
+extern path SPTable[100];
+extern unsigned char ETable[100];
+
+extern int mid;
+
+/*Função de inicialização da RTable
+ * define-se como caminho vazio um caminho de tamanho <=0*/
+void initRT();
+
+void initSPT();
+
+void initET();
+/*Retoma o destino do path ou -1 se tamanho <=0*/
+unsigned char dest(path p);
+/*Retoma o source do path ou -1 se tamanho <=0*/
+unsigned char source(path p);
+/*Função de comparação de paths
+ * Retorna -1 se p1 < p2
+ * retorna 0 se p1 = p2
+ * retorna 1 se p1 > p2
+ * retorna 2 se tamanho igual mas caminho diferente*/
+int patheq(path p1, path p2);
+/*Atualiza a RTable com o path. Verifica se é necessário atualizar a SP*/
+void updateRT(path p);
+/*Recalcula o elemento row da SPTable
+ * Se mudar envia Route*/
+void updateSP(unsigned char row);
+
+void adj_route(path p);
+
+void sendRoute(path p, int fd);
+/*Função específica para colocar paths vazios na RT.
+ * MUITO CUIDADO A USAR
+ * Perceber muito bem antes de usar.
+ * Assume que um path vazio tem src no indice 0 e dest no indice 1
+ * MUITO CUITADO A USAR*/
+void updateRT_empty(path);
+/*Função de remoção de coluna*/
+void removeCol(unsigned char id);
